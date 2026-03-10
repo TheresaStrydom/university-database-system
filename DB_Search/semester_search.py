efe30b1558e7270b73b1fbddcf8b5074d139fe75
MatthewIanRobin <81291790+MatthewIanRobin@users.noreply.github.com>
2026-03-10 21:38:20 +0200

    Update semester_search.py
    Added Doc-strings to module and functions to comply with PEP-8.

--- DB_Search/semester_search.py
+++ DB_Search/semester_search.py
@@ -1,6 +1,19 @@
+"""
+This module is used by the GUI to execute a function that will search
+the database. The function:
+
+available_semesters()
+
+The function contain a MySQL query that can be passed to the database
+via an imported query.
+"""
+
 from DB_Conn.connection import connection_context
 
 def available_semesters():
+    """
+    This function is used by the GUI to execute a function that will search for the available semesters.
+    :return: All of the available semesters.
+    """
     query = "SELECT semester_id FROM semesters"
     return connection_context(query)
-

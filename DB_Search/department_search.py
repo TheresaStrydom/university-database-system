1824c7a0d27747267861d8203b1706513cb3c7f3
MatthewIanRobin <81291790+MatthewIanRobin@users.noreply.github.com>
2026-03-10 21:36:26 +0200

    Update department_search.py
    Added Doc-strings to module and functions to comply with PEP-8.

--- DB_Search/department_search.py
+++ DB_Search/department_search.py
@@ -1,5 +1,20 @@
+"""
+This module is used by the GUI to execute a function that will search
+the database. The function is:
+
+available_departments()
+
+The function contain a MySQL query that can be passed to the database
+via an imported query.
+"""
+
 from DB_Conn.connection import connection_context
 
 def available_departments():
+    """
+    This function is used by the GUI to execute a function that will search
+    the available departments.
+    :return: The names of the departments
+    """
     query = """SELECT name FROM departments"""
     return connection_context(query)

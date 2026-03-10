5cb0e02a0ee71d663f363aa5a90c730c58043862
MatthewIanRobin <81291790+MatthewIanRobin@users.noreply.github.com>
2026-03-10 21:39:03 +0200

    Update student_search.py
    Added Doc-strings to module and functions to comply with PEP-8.

--- DB_Search/student_search.py
+++ DB_Search/student_search.py
@@ -1,6 +1,25 @@
+"""
+This module is used by the GUI to execute functions that will search
+the database. The functions are:
+
+student_search_per_course_per_lecturer()
+student_search_last_year_and_results()
+search_courses_per_department_per_lecturer()
+
+They both contain a MySQL query that can be passed to the database via
+an imported function.
+"""
+
 from DB_Conn.connection import connection_context
 
 def student_search_per_course_per_lecturer(course_code,lecturer_name):
+    """
+    This function is used by the GUI to search for courses based on given course_code and lecturer_name.
+    department and lecturer.
+    :param course_code: The course code as passed by the GUI.
+    :param lecturer_name: The name of the lecturer as passed by the GUI.
+    :return: Returns a list of students
+    """
     query = f"""
     SELECT a.name 
     FROM students AS a
@@ -12,6 +31,10 @@ def student_search_per_course_per_lecturer(course_code,lecturer_name):
     return connection_context(query)
 
 def student_search_last_year_and_results():
+    """
+    This function is used by the GUI to search for courses based on last year and results.
+    :return: All the students to achieve greater than 70% and is in the final year of study.
+    """
     query = """
     SELECT DISTINCT a.name
     FROM students AS a
@@ -23,6 +46,10 @@ def student_search_last_year_and_results():
     return connection_context(query)
 
 def student_search_not_enrolled():
+    """
+    This function is used by the GUI to search for students that have not enrolled in the current semester.
+    :return: List of students not enrolled in the current semester.
+    """
     query = """
     SELECT name 
     FROM students 
